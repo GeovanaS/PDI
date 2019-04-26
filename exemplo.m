32f8ef330bd48ad156b59791c7aafb769d799790
Geovana Silveira <geovanasilvadasilveira1@gmail.com>
2019-04-25 23:56:28 -0300

    Update exemplo.m

--- exemplo.m
+++ exemplo.m
@@ -32,18 +32,3 @@
 %endfor
 %subplot(1,2,1);imshow(ad);title('Before');
 %subplot(1,2,2);imshow(x);title('After');
-
-a = imread('cameraman.tif');
-ad = im2double(a);
-x = ad; y = ad;
-[r ,c] = size(x)
-factor = 6
-for i = 1:r
-    for j = 1:c
-      x(i,j) = factor*log(1+ad(i,j));
-      y(i,j) = factor*ad(i,j)*2;
-    endfor
-endfor
-subplot(1,2,1);imshow(ad);title('Before');
-subplot(1,2,2);imshow(x);title('After');
-figure,imshow(y);
