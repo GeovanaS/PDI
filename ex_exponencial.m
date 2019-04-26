9e24139275e0fbba69da8d6135fbaa6a508024b5
Geovana Silveira <geovanasilvadasilveira1@gmail.com>
2019-04-26 00:24:36 -0300

    Update ex_exponencial.m

--- ex_exponencial.m
+++ ex_exponencial.m
@@ -4,13 +4,13 @@ I = imread('cameraman.tif');
 subplot(2,2,1),imshow(I);
 Id = im2double(I);
 
-Ouput1 = 4*(((1+0.3).^(Id))-1);
-Ouput2 = 4*(((1+0.4).^(Id))-1);
-Ouput3 = 4*(((1+0.6).^(Id))-1);
+Output1 = 4*(((1+0.3).^(Id))-1);
+Output2 = 4*(((1+0.4).^(Id))-1);
+Output3 = 4*(((1+0.6).^(Id))-1);
 
-subplot(2,2,2),imshow(Ouput1);
-subplot(2,2,3),imshow(Ouput2);
-subplot(2,2,4),imshow(Ouput3);
+subplot(2,2,2),imshow(Output1);
+subplot(2,2,3),imshow(Output2);
+subplot(2,2,4),imshow(Output3);
 
 X = 0:255;
 %X1=0:0.0039:1;
