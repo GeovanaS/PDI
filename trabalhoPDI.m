4642410c8d709573e85e7d8de22d8b012296b73f
Geovana Silveira <geovanasilvadasilveira1@gmail.com>
2019-07-12 03:07:10 -0300

    Add files via upload

--- trabalhoPDI.m
+++ trabalhoPDI.m
@@ -1,6 +1,6 @@
-I = imread("Placas/br_aih-2194_rear.jpg");
-subplot(1,2,1),imshow(I);
-title('Imagem Original');
+I = imread("Placas/br_lbm-4000.jpg");
+subplot(2,2,1),imshow(I);
+%title('Imagem Original');
 
 IG = rgb2gray(I);
 
@@ -14,9 +14,18 @@ IM = medfilt2(IG);
 
 s = fspecial('sobel');
 sf = imfilter(IM,s,'symmetric');
+subplot(2,2,2),imshow(sf);
+
 %imshow(sf);
 
 cc = bwconncomp(sf);
+hold on
+[c,h] = contour(IG); 
+
+B = im2bw(sf,0.4);
+
+IM = medfilt2(IG);
+subplot(2,2,2),imshow(IM);
 
 BWdfill = imfill(IM,'holes');
 %imshow(BWdfill)
@@ -27,8 +36,13 @@ BWnobord = imclearborder(BWdfill,4);
 seD = strel('diamond',1);
 BWfinal = imerode(BWnobord,seD);
 BWfinal = imerode(BWfinal,seD);
+subplot(2,2,3),imshow(BWfinal);
+
 %imshow(labeloverlay(I,BWfinal))
 
-f = imsubtract(BWfinal,IG);
-subplot(1,2,2),imshow(f);
-title('Número da Placa');
+%f = imsubtract(BWfinal,IG);
+%subplot(1,2,2),imshow(f);
+f = imsubtract(BWfinal,IM);
+
+subplot(2,2,4),imshow(f);
+%title('Número da Placa');
