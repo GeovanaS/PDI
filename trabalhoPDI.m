115cc1f10fc6d1e69d1d87393161cb09beb11be7
Geovana Silveira <geovanasilvadasilveira1@gmail.com>
2019-07-12 08:53:32 -0300

    Update trabalhoPDI.m

--- trabalhoPDI.m
+++ trabalhoPDI.m
@@ -2,46 +2,36 @@ I = imread("Placas/br_lbm-4000.jpg");
 subplot(2,2,1),imshow(I);
 %title('Imagem Original');
 
+%Escala cinza
 IG = rgb2gray(I);
 
-msk = [0 0 0 0 0;
-       0 1 1 1 0;
-       0 1 1 1 0;
-       0 0 0 0 0;];
-
-IM = medfilt2(IG);
-%imshow(IM);
-
+%Aplica o filtro de sobel para detectar as bordas
 s = fspecial('sobel');
 sf = imfilter(IM,s,'symmetric');
 subplot(2,2,2),imshow(sf);
 
-%imshow(sf);
-
-cc = bwconncomp(sf);
-hold on
-[c,h] = contour(IG); 
-
+%Binariza a imagem
 B = im2bw(sf,0.4);
 
+% Aplica o filtro da media
 IM = medfilt2(IG);
 subplot(2,2,2),imshow(IM);
 
+% Filtro
 BWdfill = imfill(IM,'holes');
 %imshow(BWdfill)
 
+% Limpa as bordas
 BWnobord = imclearborder(BWdfill,4);
 %imshow(BWnobord)
 
+%Erosão
 seD = strel('diamond',1);
 BWfinal = imerode(BWnobord,seD);
 BWfinal = imerode(BWfinal,seD);
 subplot(2,2,3),imshow(BWfinal);
 
-%imshow(labeloverlay(I,BWfinal))
-
-%f = imsubtract(BWfinal,IG);
-%subplot(1,2,2),imshow(f);
+% Retorna a diferença entre a imagem erosada e a imagem filtrada
 f = imsubtract(BWfinal,IM);
 
 subplot(2,2,4),imshow(f);
